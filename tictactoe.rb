6d8c6eadf056d3204b0e46c4c831e33972d7eee3
Bhavishya singh <bhavishya.k.singh@gmail.com>
2017-01-21 21:05:24 +0530

    Update tictactoe.rb

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,11 +1,11 @@
 class Player
 	@name
 	@symbol
-	def initialize(id)
-		puts "Enter the name of #{id}st player :"
-		@name = gets
-		puts "Enter the symbol of #{id}st player :"
-		@symbol = gets
+	def initialize(name,symbol)
+		#puts "Enter the name of #{id}st player :"
+		@name = name
+		#puts "Enter the symbol of #{id}st player :"
+		@symbol = symbol
 	end
 	def getName
 		@name
@@ -21,10 +21,10 @@ class Board
 		@board =[]
 		@size = size
 		j= 0
-		while(j<5)do
+		while(j<size)do
 			val =[]	
 			i =0 
-			while(i<5)do
+			while(i<@size)do
 				val.push(0)
 				i=i+1
 			end
@@ -35,6 +35,7 @@ class Board
 	def isSafe(x,y)
 		if(@board[x][y]!=0)
 			return false
+		end
 		return true
 	end
 	def set(x, y, symbol)
@@ -42,58 +43,67 @@ class Board
 	end
 	def ifwin symbol
 		i = 0
-		while(i<size)do
+		while(i<@size)do
 			check = true
 			j=0
-			while(j<size)do
+			while(j<@size)do
 				if(@board[i][j]!=symbol)
 					check = false
-					j=j+1
-			end
-			if(check)
-				return true
-			i=i+1
-		end
-		while(i<size)do
-			check = true
-			j=0
-			while(j<size)do
-				if(@board[j][i]!=symbol)
-					check = false
+				end
 				j=j+1
 			end
 			if(check)
 				return true
+			end
+			i=i+1
+		end
+		i = 0;
+		while(i<@size)do
+			check = true
+			j=0
+			while(j<@size)do
+				if(@board[j][i]!=symbol)
+					check = false
+				end
+				j=j+1
+			end
+			if(check)
+				return true
+			end
 			i=i+1
 		end
 		i=0
 		check =true
 		j=0
-		while(i<size)do
+		while(i<@size)do
 			if(@board[i][j]!=symbol)
 				check=false
+			end
 			i=i+1
 			j=j+1
 		end
 		if(check)
 			return true
+		end
 		i=0
 		check =true
-		j=size-1
-		while(i<size)do
+		j=@size-1
+		while(i<@size)do
 			if(@board[i][j]!=symbol)
 				check=false
+			end
 			i=i+1
 			j=j-1
 		end
 		if(check)
 			return true
+		end
 		return false
 	end
 	def showboard
 		i = 0
-		while (i<size) do
-			print board[i]
+		while (i<@size) do
+			print @board[i]
 			puts
 			i=i+1
 		end
@@ -107,37 +117,47 @@ class Game
 	@count 
 	def initialize(n)
 		@board = Board.new(n)
-		@player1 = Player.new("1")
-		@player2 = Player.new("2")
+		@player1 = Player.new("anand","X")
+		@player2 = Player.new("bhavishya","O")
 		@curplayer = 1
 		@count =0
 	end
 	def changeplayer
-		if curplayer==1
-			curplayer=2
+		if @curplayer==1
+			@curplayer=2
 		else
-			curplayer=1
+			@curplayer=1
+		end
 
 	end
 	def doeswin
-		if curplayer==1
-			return ifwin(@player1.getSymbol)
-		return ifwin(@player2.getSymbol)
+			return @board.ifwin(@player1.getSymbol) || @board.ifwin(@player2.getSymbol)
+	end
+	def doesFirstWin
+		return @board.ifwin(@player1.getSymbol)
+	end
+	def getFirstPlayer
+		return @player1.getName
+	end
+	def getSecondPlayer
+		return @player2.getName
 	end
 	def currentPlayer
-		if curplayer==1
+		if @curplayer==1
 			return @player1.getName
+		end
 		return @player2.getName
 	end
 	def isSafeMove(x, y)
-		isSafe(x,y)
+		@board.isSafe(x,y)
 	end
 	def setMove(x, y)
-		if(curplayer==1)
-			set(x,y,@player1.getSymbol)
+		if(@curplayer==1)
+			@board.set(x,y,@player1.getSymbol)
 		else
-			set(x,y,@player2.getSymbol)
-			@count = @count + 1
+			@board.set(x,y,@player2.getSymbol)
+		end
+		@count = @count + 1
 	end
 	def getCount
 		@count
@@ -149,29 +169,37 @@ end
 class GameManager
 	puts "Welcome"
 	puts "please specify the size of the board"
-	size = gets.to_i
-	game = Game.new(size)
-	while(!game.doeswin &&game.getCount<9)do
+	#size = gets.to_i
+	game = Game.new(3)
+	while(!game.doeswin && game.getCount<9)do
 		valid = false
+		puts "it is #{game.currentPlayer} chance :"
 		while(!valid)do
-		puts ="enter X"
-		x = gets.to_i
-		puts = "enter y"
-		y = gets.to_i
-		if isSafeMove x y
-			valid = true
-			game.setMove x y
-		else
-			puts "invalid please enter again"
-		
+			puts "enter X"
+			x = gets.to_i
+			puts "enter y"
+			y = gets.to_i
+			if game.isSafeMove(x, y)
+				valid = true
+				game.setMove(x,y)
+			else
+				puts "invalid please enter again"
+			end
 		end
+		puts "been here"
 		game.changeplayer
+		system("clear")
 		game.show
 	end
 	if game.doeswin
-		puts "#{} winsssssssssssssssssssssssssssssssss"
+		if game.doesFirstWin
+			puts "***********************************#{game.getFirstPlayer} Wins *************************************************"
+		else
+			puts "***********************************#{game.getSecondPlayer} Wins *************************************************"
+		end
 	else
 		puts "Draw"
+	end
 end
 
 
